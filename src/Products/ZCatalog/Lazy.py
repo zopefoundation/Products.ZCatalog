cbc93b878c2603e9cace48b1a30943402381ec38
Hanno Schlichting <hanno@hannosch.eu>
2010-12-25 19:15:27 +0000

    Merged c118895, c118896 from Zope 2.13 branch - these didn't make it into Zope2 trunk which was the base for the spin-off

--- src/Products/ZCatalog/Lazy.py
+++ src/Products/ZCatalog/Lazy.py
@@ -145,26 +145,20 @@ class LazyMap(Lazy):
     # Don't access data until necessary
 
     def __init__(self, func, seq, length=None):
-        self._seq=seq
-        self._func=func
+        self._seq = seq
+        self._data = {}
+        self._func = func
         if length is not None:
-            self._len=length
+            self._len = length
         else:
             self._len = len(seq)
         self.actual_result_count = self._len
-        self._marker = object()
-        self._data = [self._marker] * self._len
 
-    def __getitem__(self,index):
-        data=self._data
-        try:
-            s=self._seq
-        except AttributeError:
+    def __getitem__(self, index):
+        data = self._data
+        if index in data:
             return data[index]
-
-        value = data[index]
-        if value is self._marker:
-            value = data[index] = self._func(s[index])
+        value = data[index] = self._func(self._seq[index])
         return value
 
 
