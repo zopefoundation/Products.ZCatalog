d338be8a6ef1dbe71830347d14b569a933e1f834
Hanno Schlichting <hanno@hannosch.eu>
2011-04-01 11:04:09 +0000

    Prepare Products.ZCatalog 2.13.8.

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@
 from setuptools import setup, find_packages
 
 setup(name='Products.ZCatalog',
-      version = '2.13.8dev',
+      version = '2.13.8',
       url='http://pypi.python.org/pypi/Products.ZCatalog',
       license='ZPL 2.1',
       description="Zope 2's indexing and search solution.",
