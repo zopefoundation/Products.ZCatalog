3f9afd4c9da80e232bd9ca1c113183d24147c28c
Hanno Schlichting <hanno@hannosch.eu>
2013-10-15 11:41:37 +0200

    vb

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@
 from setuptools import setup, find_packages
 
 setup(name='Products.ZCatalog',
-      version = '2.13.24',
+      version = '2.13.25dev',
       url='http://pypi.python.org/pypi/Products.ZCatalog',
       license='ZPL 2.1',
       description="Zope 2's indexing and search solution.",
