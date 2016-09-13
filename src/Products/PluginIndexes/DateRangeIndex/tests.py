6d1f0663e20e6032d86613de8abb28c5a0e0e3c0
Andreas Gabriel <gabriel@hrz.uni-marburg.de>
2016-09-13 14:12:04 +0200

    Fix function/property call

--- src/Products/PluginIndexes/DateRangeIndex/tests.py
+++ src/Products/PluginIndexes/DateRangeIndex/tests.py
@@ -332,7 +332,7 @@ class DateRangeIndexTests(unittest.TestCase):
         for value in range(-1, 15):
             matches = matchingDummiesByTimeValue(value, precision)
             results, used = self._checkApply(index, {'work': value}, matches)
-            matches = sorted(matches, key=lambda d: d[1].name)
+            matches = sorted(matches, key=lambda d: d[1].name())
             for result, match in map(None, results, matches):
                 datum = map(lambda d: convertDateTime(d, precision),
                             match[1].datum())
