43c332aa9616024d0992068d874b11e32d690358
Jens Vagelpohl <jens@netz.ooo>
2017-05-15 20:00:29 +0200

    Remove PyPy classifier [ci skip]

--- setup.py
+++ setup.py
@@ -41,7 +41,6 @@ setup(
         "Programming Language :: Python :: 3.5",
         "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: Implementation :: CPython",
-        "Programming Language :: Python :: Implementation :: PyPy",
         "Topic :: Internet :: WWW/HTTP :: Indexing/Search",
     ],
     install_requires=[
