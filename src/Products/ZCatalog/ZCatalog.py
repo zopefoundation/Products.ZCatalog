41871ec5f4c90ea54b9aac1c3569b8d2076e8fc2
Dr. Frank Hoffmann <Dr. Frank Hoffmann@HAL01.fritz.box>
2018-06-20 01:39:59 +0200

    cleaned HTML/icons, fixed object insertation url for modal dialog

--- src/Products/ZCatalog/ZCatalog.py
+++ src/Products/ZCatalog/ZCatalog.py
@@ -99,7 +99,7 @@ class ZCatalog(Folder, Persistent, Implicit):
     security.declareProtected(search_zcatalog, 'all_meta_types')
 
     meta_type = 'ZCatalog'
-    zmi_icon = 'fas fa-search'
+    zmi_icon = 'fa fa-search'
 
     manage_options = (
         {'label': 'Contents', 'action': 'manage_main'},
