6f3b4af07e70da70bd087418829e172398ed305b
Michael Howitz <mh@gocept.com>
2019-01-29 13:50:01 +0100

    Preparing release 2.13.30

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@
 from setuptools import setup, find_packages
 
 setup(name='Products.ZCatalog',
-      version='2.13.30.dev0',
+      version='2.13.30',
       url='http://pypi.python.org/pypi/Products.ZCatalog',
       license='ZPL 2.1',
       description="Zope 2's indexing and search solution.",
