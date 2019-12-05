4650aec9ab49dbbe9a65f116f7d5809bc09eca6f
Michael Howitz <mh@gocept.com>
2019-12-05 16:48:13 +0100

    Do not break when indexing multiple indexes

--- src/Products/ZCatalog/ZCatalog.py
+++ src/Products/ZCatalog/ZCatalog.py
@@ -411,13 +411,15 @@ class ZCatalog(Folder, Persistent, Implicit):
     @security.protected(manage_zcatalog_entries)
     def reindexIndex(self, name, REQUEST, pghandler=None):
         if isinstance(name, str):
-            name = (name, )
+            idxs = (name, )
+         else:
+            idxs = name
 
         paths = self._catalog.uids.keys()
 
         i = 0
         if pghandler:
-            pghandler.init('reindexing %s' % name, len(paths))
+            pghandler.init('reindexing {}'.format(idxs), len(paths))
 
         for p in paths:
             i += 1
@@ -433,7 +435,7 @@ class ZCatalog(Folder, Persistent, Implicit):
             else:
                 # don't update metadata when only reindexing a single
                 # index via the UI
-                self.catalog_object(obj, p, idxs=name,
+                self.catalog_object(obj, p, idxs=idxs,
                                     update_metadata=0, pghandler=pghandler)
 
         if pghandler:
