7aee53b23a3eb62e17f4dfb41f36c63279c3542e
Michael Howitz <icemac@gmx.net>
2025-10-01 08:48:41 +0200

    Back to development: 7.3

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ from setuptools import setup
 
 setup(
     name='Products.ZCatalog',
-    version='7.2.1',
+    version='7.3.dev0',
     url='https://github.com/zopefoundation/Products.ZCatalog',
     project_urls={
         'Issue Tracker': 'https://github.com/zopefoundation/'
