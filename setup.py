29002e40eee7df76a073dbf2ad610b7cfa2bee9a
Jens Vagelpohl <jens@plyp.com>
2025-11-22 10:24:09 +0100

    - vb [ci skip]

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ from setuptools import setup
 
 setup(
     name='Products.ZCatalog',
-    version='7.3',
+    version='7.4.dev0',
     url='https://github.com/zopefoundation/Products.ZCatalog',
     project_urls={
         'Issue Tracker': 'https://github.com/zopefoundation/'
